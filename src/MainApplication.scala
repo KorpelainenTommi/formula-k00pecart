d86c120298ee2dcbe51f5544e402c128e37a2b11
Tommi Korpelainen <tommi.korpelainen@aalto.fi>
2021-02-19 00:29:01 +0200

    KeyEvent handling test

--- src/MainApplication.scala
+++ src/MainApplication.scala
@@ -1,8 +1,8 @@
 package formula
 
-import java.awt.Graphics
+import java.awt.event.KeyEvent
+import java.awt.{Dimension, Graphics, KeyEventDispatcher}
 import javax.swing._
-import java.awt.Dimension
 object MainApplication extends App {
 
   val topWindow = new JFrame("K00PECART")
@@ -12,5 +12,20 @@ object MainApplication extends App {
   topWindow.setResizable(false)
   topWindow.add(new JLabel("Hello formula", SwingConstants.CENTER){ this.setFont(this.getFont.deriveFont(32F)) })
   topWindow.setVisible(true)
+
+  //Example of globally catching key events
+  java.awt.KeyboardFocusManager.getCurrentKeyboardFocusManager.addKeyEventDispatcher(new KeyEventDispatcher {
+    override def dispatchKeyEvent(e: KeyEvent): Boolean = {
+      if(e.getID == java.awt.event.KeyEvent.KEY_PRESSED) {
+      JOptionPane.showMessageDialog(topWindow, "Key: " + e.getKeyChar)
+      }
+      false
+    }
+  })
+
+  while(true) {
+    //This should not block keyhandling
+  }
+
 
 }
